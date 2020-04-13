ac8dc0c6aa5d6b5d9192104b6717456b3424cee1
04052474 <37133566+04052474@users.noreply.github.com>
2020-04-13 15:47:03 +0800

    Add files via upload

---
+++ postback01.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="postback01.aspx.cs" Inherits="postback01" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Button" Width="65px" />
+    <p>
+        <input id="Submit1" type="submit" value="submit" /></p>
+    </form>
+</body>
+</html>
---
+++ postback01.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class postback01 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            Response.Write("第一次載入<br>");
+        }
+        else
+        {
+            int x = 1;
+            int y = 1;
+            Response.Write("網頁重新PostBack <br>");
+            Response.Write("九九乘法表");
+            Response.Write("<table border=4>");
+            for (x = 1; x <= 9; x++)
+            {
+                Response.Write("<tr>");
+                for (y = 1; y <= 9; y++)
+                {
+                Response.Write("<th > "+x+"*"+y+"="+x*y+" </th >");
+                }
+                Response.Write("</ tr>");
+
+            }
+            Response.Write("</table>");
+        }
+    }
+}
